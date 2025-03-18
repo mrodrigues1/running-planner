87a9bd37480a2a1090c203a16fd2f48a92cf4527
mrodrigues1 <i&@gqgvZVsnlUxeMX1X1rqyzBUOI7^LpkoM55aic3lENC!nQXq>
2025-03-18 14:44:43 -0300

    test: add tests for run-walk interval workout creation
    Added unit tests for `WorkoutExtensions.CreateRunWalkWorkout` to verify
    correct estimated time calculation. This includes tests for single and
    multiple walk-run intervals.

--- RunningPlanner.Tests/WorkoutTests.cs
+++ RunningPlanner.Tests/WorkoutTests.cs
@@ -1,4 +1,5 @@
-﻿using RunningPlanner.Core.Models;
+﻿using RunningPlanner.Core.Extensions;
+using RunningPlanner.Core.Models;
 
 namespace RunningPlanner.Tests;
 
@@ -9,7 +10,7 @@ public class WorkoutTests
     {
         // Arrange
         var easyPace = (TimeSpan.FromMinutes(6), TimeSpan.FromMinutes(6).Add(TimeSpan.FromSeconds(30)));
-        
+
         var restPace = (TimeSpan.FromMinutes(7), TimeSpan.FromMinutes(10));
 
         var workoutBuilder = Workout.WorkoutBuilder
@@ -33,5 +34,44 @@ public class WorkoutTests
 
         // Assert
         Assert.Equal(8.7m, workout.TotalDistance.DistanceValue);
+    }
+
+    [Fact]
+    public void Workout_WhenCreatedWithWalkRunIntervals_ThenEstimatedTimeIs18M()
+    {
+        // Arrange
+        var easyPace = (TimeSpan.FromMinutes(6), TimeSpan.FromMinutes(6).Add(TimeSpan.FromSeconds(30)));
+
+        var restPace = (TimeSpan.FromMinutes(7), TimeSpan.FromMinutes(10));
+
+        // Act
+        var workout = WorkoutExtensions
+            .CreateRunWalkWorkout(
+                easyPace,
+                restPace,
+                [new WalkRunInterval(9, TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1))]);
+
+        // Assert
+        Assert.Equal(workout.EstimatedTime, TimeSpan.FromMinutes(18));
+    }
+    
+    [Fact]
+    public void Workout_WhenCreatedWithMultipleWalkRunIntervals_ThenEstimatedTimeIs30M()
+    {
+        // Arrange
+        var easyPace = (TimeSpan.FromMinutes(6), TimeSpan.FromMinutes(6).Add(TimeSpan.FromSeconds(30)));
+
+        var restPace = (TimeSpan.FromMinutes(7), TimeSpan.FromMinutes(10));
+
+        // Act
+        var workout = WorkoutExtensions
+            .CreateRunWalkWorkout(
+                easyPace,
+                restPace,
+                new WalkRunInterval(9, TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1)),
+                new WalkRunInterval(3, TimeSpan.FromMinutes(2), TimeSpan.FromMinutes(2)));
+
+        // Assert
+        Assert.Equal(workout.EstimatedTime, TimeSpan.FromMinutes(30));
     }
 }
